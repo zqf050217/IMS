d50c9ef5597a30e560f93a69b7e86fcdba962dab
gugia <gugia@LX-LAPTOP-C7K>
2017-08-21 17:13:49 +0800

    Activiti integration.

---
+++ src/main/java/com/bc/springdatajpa/OnboardingRequest.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2017 BangChen Information Technology Ltd., Co.
+ * Licensed under the Apache License 2.0.
+ */
+package com.bc.springdatajpa;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+
+/**
+ * Activiti quick start
+ *
+ * @author L.X <gugia@qq.com>
+ */
+public class OnboardingRequest {
+
+    public static void main(String[] args) {
+        ProcessEngineConfiguration cfg = new StandaloneProcessEngineConfiguration()
+                .setJdbcUrl("jdbc:h2:mem:activiti;DB_CLOSE_DELAY=1000")
+                .setJdbcUsername("sa")
+                .setJdbcPassword("")
+                .setJdbcDriver("org.h2.Driver")
+                .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+        ProcessEngine processEngine = cfg.buildProcessEngine();
+        String pName = processEngine.getName();
+        String ver = ProcessEngine.VERSION;
+        System.out.println("ProcessEngine [" + pName + "] Version: [" + ver + "]");
+    }
+}
